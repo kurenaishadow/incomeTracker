601f81aadb4189f3df0b73e2b8809e6dc932ecc1
lryuzaki <geni.miryokuteki@gmail.com>
2025-09-13 21:34:40 +0800

    changes in dashboard and savings

--- dashboard.php
+++ dashboard.php
@@ -644,7 +644,7 @@ $conn->close();
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
     <title>Dashboard - Business Dashboard</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
