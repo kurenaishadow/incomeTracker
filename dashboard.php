8add0a668b1d0720963e8110ec79f388df371454
lryuzaki <geni.miryokuteki@gmail.com>
2025-09-13 21:39:17 +0800

    done editing dashboard and savings with mobile view

--- dashboard.php
+++ dashboard.php
@@ -644,7 +644,8 @@ $conn->close();
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=0"> -->
+    <meta name="viewport" content="width=1100">
     <title>Dashboard - Business Dashboard</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
--- savings.php
+++ savings.php
@@ -208,7 +208,7 @@ $logs_result = $conn->query("SELECT * FROM savings_logs WHERE user_id = $user_id
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Savings Management</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
